9b32a6d0c32d68f1e00942f0f8944a530ef118ae
Andrii Litvin <al@gogmat.com>
2018-08-02 18:26:36 +0300

    fix

--- src/js/component.js
+++ src/js/component.js
@@ -1,6 +1,37 @@
-$(document).ready(function() { 
-  $('.head-img img').click(function(){
-    var destination = $(".program").offset().top - 0;
-    $("body,html").animate({ scrollTop: destination}, 500 );
+$(document).ready(function () {
+  $('.head-carousel').owlCarousel({
+    loop: true,
+    margin: 0,
+    nav: false,
+    items: 1
   });
+  $('.team-carousel').owlCarousel({
+    loop: true,
+    nav: true,
+    responsive: {
+      0: {
+        items: 1,
+        
+      },
+      600: {
+        items: 3,
+      }
+    }
+  });
+
+  $(window).scroll(function () {
+    return $('nav').toggleClass("fixed", $(window).scrollTop() > 0);
+  });
+  
+  $('.team-carousel .owl-next').hover(function(){
+    $triggerClick = setInterval(function(){
+        $('.team-carousel .owl-next').trigger('click');
+      }, 1000)
+  }, function() {
+    clearInterval($triggerClick);
+  })
+  
+
+  
+
 });
